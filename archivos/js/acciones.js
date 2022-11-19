66ad5efe786be77361e1a095d38497b1e2a3541f
KevinMartinezMella <iamkamm7@gmail.com>
2022-11-19 14:33:40 -0300

    Ficha Terminada

--- archivos/js/acciones.js
+++ archivos/js/acciones.js
@@ -37,11 +37,49 @@ $(document).ready(function(){
         $('.contenido').fadeOut();
         setTimeout(function(){ $('.resumenFinal').fadeIn("slow"); }, 1000);
     })
+
+    $('#registrarNino').click(function(){
+        var nombreNino = $('#nombreNino').val();
+        var edadNino = $('#edadNino').val();
+        var generoNino = $('#generoNino').val();
+        var fechaNino = $('#fechaNino').val();
+        if(generoNino == 'Niño'){
+            $('.ninoAgregado').html(`
+            <center>
+                <img class="imgNino" src="archivos/img/nino.jpg" alt="">
+            </center>
+            <br>
+            <p class="azul"><b>Nombre: ${nombreNino}</b></p>
+            <p class="azul"><b>Edad: ${edadNino} años</b></p>
+            <p class="azul"><b>Género: ${generoNino}</b></p>
+            <p class="azul"><b>Nacimiento: ${fechaNino}</b></p>
+            <a href="menu.html" class="btn btn-primary btn-block">Aceptar</a>`);
+        } else {
+            $('.ninoAgregado').html(`
+            <center>
+                <img class="imgNino" src="archivos/img/nina.jpg" alt="">
+            </center>
+            <br>
+            <p class="azul"><b>Nombre: ${nombreNino}</b></p>
+            <p class="azul"><b>Edad: ${edadNino} años</b></p>
+            <p class="azul"><b>Género: ${generoNino}</b></p>
+            <p class="azul"><b>Nacimiento: ${fechaNino}</b></p>
+            <a href="menu.html" class="btn btn-primary btn-block">Aceptar</a>`);
+        }
+        $('.formFicha').fadeOut()
+        setTimeout(function(){ $('.ninoAgregado').fadeIn(); }, 500);
+
+    });
+
+    $('.agregarB').click(function(){
+        $('.agregarB').fadeOut();
+        setTimeout(function(){ $('.formFicha').fadeIn(); }, 500);
+    })
 });
 
 $(function(){
     var reservar = 'reservar.html';
-    var grabaciones = 'grabaciones.html';
+    var horas = 'horas.html';
     var camaras = 'camaras.html';
     var profesionales = 'profesionales.html';
     var ficha = 'ficha.html';
@@ -53,5 +91,9 @@ $(function(){
     $('.reservar').click(function(){
         cargador();
         setTimeout(function(){ redireccionar(reservar) }, 1000);
+    });
+    $('.ficha').click(function(){
+        cargador();
+        setTimeout(function(){ redireccionar(ficha) }, 1000);
     });
 })
